2f7d3cfac56cc13c53190ab6c27b394e076d7da4
machakux <bmachaku@gmail.com>
2015-02-12 13:32:39 +0300

    first commit

---
+++ app/src/main/java/cd/education/data/collector/android/exception/GeoPointNotFoundException.java
@@ -0,0 +1,14 @@
+package cd.education.data.collector.android.exception;
+
+public class GeoPointNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public GeoPointNotFoundException (String msg) {
+		super(msg);
+	}
+
+}
